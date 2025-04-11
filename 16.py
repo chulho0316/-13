19c2d8fb2f3d9cb8508511c85f0a6cd296e987c5
chulho0316 <ch1kb@naver.com>
2025-04-11 08:34:34 +0000

    20250411 연습 파일 추가

---
+++ 16.py
@@ -0,0 +1,7 @@
+# inport -> 동적 라이브러리 호출(d11)
+# include -> (C, c++언어) 정적 라이브러리 호출(s11)
+# import tensorflow
+# print(tensorflow.__name)
+
+from fibonacciModule import fibonacci as fib4
+print(fib4(100))
